67657da9bd8a353e90feb5e5e6b7f06e7931526e
Aaron Rono <ronolikeaaron90@gmail.com>
2019-08-25 22:00:18 +0300

    initial commit

---
+++ src/interview_preparation_kit/warmup_challenges/SockMerchant.kt
@@ -0,0 +1,22 @@
+package interview_preparation_kit.warmup_challenges
+
+import java.util.*
+
+/***
+ * Solution  to https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+ * */
+fun sockMerchant(n: Int, ar: Array<Int>): Int {
+    return 0
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val ar = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
+
+    val result = sockMerchant(n, ar)
+
+    println(result)
+}
