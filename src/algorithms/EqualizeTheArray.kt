632a4e8586d8f0ec963a184c34af31b92c9017b8
Aaron Rono <ronolikeaaron90@gmail.com>
2019-08-27 12:13:47 +0300

    EqualizeTheArray cleanup

--- src/algorithms/EqualizeTheArray.kt
+++ src/algorithms/EqualizeTheArray.kt
@@ -1,25 +1,7 @@
 package algorithms
 
-import java.io.*
-import java.math.*
-import java.security.*
-import java.text.*
 import java.util.*
-import java.util.concurrent.*
-import java.util.function.*
-import java.util.regex.*
-import java.util.stream.*
-import kotlin.collections.*
-import kotlin.comparisons.*
-import kotlin.io.*
-import kotlin.jvm.*
-import kotlin.jvm.functions.*
-import kotlin.jvm.internal.*
-import kotlin.ranges.*
-import kotlin.sequences.*
-import kotlin.text.*
 
-// Complete the equalizeArray function below.
 fun equalizeArray(arr: Array<Int>): Int {
     //Group the elements by number of occurrence
     val numbersByElement = arr.groupingBy { it }.eachCount()
@@ -29,8 +11,8 @@ fun equalizeArray(arr: Array<Int>): Int {
 
     var deletions = 0
     //finally loop through other elements occurrence count while adding them to counter
-    for(items in numbersByElement){
-        if(items.key != mostOccurringKey){
+    for (items in numbersByElement) {
+        if (items.key != mostOccurringKey) {
             deletions += items.value
         }
     }
@@ -40,12 +22,8 @@ fun equalizeArray(arr: Array<Int>): Int {
 
 fun main(args: Array<String>) {
     val scan = Scanner(System.`in`)
-
-    val n = scan.nextLine().trim().toInt()
-
-    val arr = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
-
+    //val n = scan.nextLine().trim().toInt()
+    val arr = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
     val result = equalizeArray(arr)
-
     println(result)
 }
